a9ac132ffcf06c3d4b3a4ea0d64a8325d98d8361
Jaanus Siim <jaanus@jaanussiim.com>
2014-03-26 08:08:09 +0200

    Initial project files

---
+++ GameKitDemo/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  GameKitDemo
+//
+
+//  Copyright (c) 2014 Coodly LLC. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ GameKitDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  GameKitDemo
+//
+
+//  Copyright (c) 2014 Coodly LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end
